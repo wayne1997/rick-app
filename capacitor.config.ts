f7abda8a56763eb742132a76988fbe72388968c3
wayne1997 <jonathan.chiriboga@upec.edu.ec>
2022-10-31 15:40:19 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rick-morthy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
